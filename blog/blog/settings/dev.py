1bd876c6d0a71e207d00e4e3821f35435019f6de
freeeakn <1rycont@gmail.com>
2023-11-14 03:07:20 +0700

    init cms and react

---
+++ blog/blog/settings/dev.py
@@ -0,0 +1,21 @@
+from .base import *
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = str(os.getenv('KEY'))
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
