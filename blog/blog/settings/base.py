49137b166ff75345139fbd449f36c12d7096e73e
root <root@localhost.localdomain>
2023-11-14 21:56:50 +0700

    fix base.py

--- blog/blog/settings/base.py
+++ blog/blog/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
