8c2bd76b05cee4ee6bf82cd9c6a3dd144d0c1112
freeeakn <1rycont@gmail.com>
2023-11-14 20:21:47 +0700

    add backend

--- backend/backend/settings/dev.py
+++ backend/backend/settings/dev.py
@@ -1,13 +1,10 @@
 from .base import *
-import os
-from dotenv import load_dotenv
-load_dotenv()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = str(os.getenv('KEY'))
+SECRET_KEY = "django-insecure-z=o=a#zg)x(r#__!8!o9mcpe7y5yt3yuu^sj&qkyd!t)zpv#hf"
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ["*"]
--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for blog project.
+WSGI config for backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blog.settings.dev")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings.dev")
 
 application = get_wsgi_application()
