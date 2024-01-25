ee6763af98e4f4a0c4a1d58995ddf4539ab29681
freeeakn <1rycont@gmail.com>
2024-01-25 15:34:20 +0700

    fix helmet titles

--- frontend/src/routes/About/AboutPage.jsx
+++ frontend/src/routes/About/AboutPage.jsx
@@ -1,6 +1,17 @@
+import { Helmet } from "react-helmet-async";
+
 function AboutPage() {
     return (
         <>
+        <Helmet title="About">
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+            <meta name="msapplication-TileColor" content="#da532c" />
+            <meta name="theme-color" content="#ffffff" />
+        </Helmet>
         <p className="text-4xl text-white"></p>
         </>
     );
