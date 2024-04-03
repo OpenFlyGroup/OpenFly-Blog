2e0d9c0616e5b0db0bd4c2e72171f040ea278756
Routybor <routybor@gmail.com>
2024-04-03 16:48:45 +0700

    raw signin modal window

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -4,6 +4,7 @@ import isActive from '@/utils/isActive'
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import SignInForm from '@/components/ui/Form/SignInForm/SignInForm'
 
 const Header: React.FC = () => {
   const pathname = usePathname()
@@ -74,64 +75,69 @@ const Header: React.FC = () => {
         </ul>
       </div>
       <div className='navbar-end gap-6'>
-          <label className='cursor-pointer grid place-items-center'>
-            <input
-              type='checkbox'
-              aria-label='dark'
-              value='dark'
-              className='toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2'
-            />
-            <svg
-              className='col-start-1 row-start-1 stroke-base-100 fill-base-100'
-              xmlns='http://www.w3.org/2000/svg'
-              width='14'
-              height='14'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            >
-              <circle cx='12' cy='12' r='5' />
-              <path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' />
-            </svg>
-            <svg
-              className='col-start-2 row-start-1 stroke-base-100 fill-base-100'
-              xmlns='http://www.w3.org/2000/svg'
-              width='14'
-              height='14'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              strokeWidth='2'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            >
-              <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
-            </svg>
-          </label>
+        <label className='cursor-pointer grid place-items-center'>
+          <input
+            type='checkbox'
+            aria-label='dark'
+            value='dark'
+            className='toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2'
+          />
+          <svg
+            className='col-start-1 row-start-1 stroke-base-100 fill-base-100'
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='14'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <circle cx='12' cy='12' r='5' />
+            <path d='M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4' />
+          </svg>
+          <svg
+            className='col-start-2 row-start-1 stroke-base-100 fill-base-100'
+            xmlns='http://www.w3.org/2000/svg'
+            width='14'
+            height='14'
+            viewBox='0 0 24 24'
+            fill='none'
+            stroke='currentColor'
+            strokeWidth='2'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+          >
+            <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
+          </svg>
+        </label>
         <ul className='menu menu-horizontal bg-base-100 p-1 rounded-full gap-2'>
-          <li>
-            <Link
-              className={`${isActive('/signin', pathname) ? 'active' : ' '} rounded-full text-sm font-[500] leading-6 text-white hover:text-primary duration-200`}
-              // href={user ? '/profile' : '/signin'}
-              href={'/signin'}
+          <button
+            className='btn'
+            onClick={() => document.getElementById('my_modal_2').showModal()}
+          >
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 24 24'
+              fill='currentColor'
+              className='w-6 h-6'
             >
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                viewBox='0 0 24 24'
-                fill='currentColor'
-                className='w-6 h-6'
-              >
-                <path
-                  fillRule='evenodd'
-                  d='M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
-                  clipRule='evenodd'
-                />
-              </svg>
-            </Link>
-          </li>
+              <path
+                fillRule='evenodd'
+                d='M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
+                clipRule='evenodd'
+              />
+            </svg>
+          </button>
+          <dialog id='my_modal_2' className='modal'>
+            <div className='modal-box w-full rounded-2xl shadow-2xl md:mt-0 sm:max-w-md xl:p-0 bg-base-200'>
+              <SignInForm></SignInForm>
+            </div>
+            <form method='dialog' className='modal-backdrop'>
+              <button></button>
+            </form>
+          </dialog>
         </ul>
       </div>
     </header>
