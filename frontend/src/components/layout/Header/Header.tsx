f52e638819822bac4f0610f6647f0934d13be26b
Routybor <routybor@gmail.com>
2024-04-07 13:21:28 +0700

    modal with headlessui

--- frontend/src/components/layout/Header/Header.tsx
+++ frontend/src/components/layout/Header/Header.tsx
@@ -8,8 +8,18 @@ import { usePathname } from 'next/navigation'
 import { useContext } from 'react'
 import SignInForm from '@/components/ui/Form/SignInForm/SignInForm'
 import SignInModal from '@/components/ui/Form/SignInForm/SignInModal'
+import { useState } from 'react'
 
 const Header: React.FC = () => {
+  let [isOpen, setIsOpen] = useState(false)
+
+  function closeModal() {
+    setIsOpen(false)
+  }
+
+  function openModal() {
+    setIsOpen(true)
+  }
   const { store } = useContext(Context)
   const pathname = usePathname()
   const NavLinks: TypeNavLink[] = [
@@ -116,7 +126,27 @@ const Header: React.FC = () => {
             <path d='M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z'></path>
           </svg>
         </label>
-        <SignInModal />
+        <button className='btn' onClick={openModal}>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            viewBox='0 0 24 24'
+            fill='currentColor'
+            className='w-6 h-6'
+          >
+            <path
+              fillRule='evenodd'
+              d='M18.685 19.097A9.723 9.723 0 0 0 21.75 12c0-5.385-4.365-9.75-9.75-9.75S2.25 6.615 2.25 12a9.723 9.723 0 0 0 3.065 7.097A9.716 9.716 0 0 0 12 21.75a9.716 9.716 0 0 0 6.685-2.653Zm-12.54-1.285A7.486 7.486 0 0 1 12 15a7.486 7.486 0 0 1 5.855 2.812A8.224 8.224 0 0 1 12 20.25a8.224 8.224 0 0 1-5.855-2.438ZM15.75 9a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z'
+              clipRule='evenodd'
+            />
+          </svg>
+        </button>
+        <div>
+          <SignInModal
+            isOpen={isOpen}
+            closeModal={closeModal}
+            openModal={openModal}
+          />
+        </div>
       </div>
     </header>
   )
